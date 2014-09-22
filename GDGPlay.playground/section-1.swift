269d2d60e6fd2520be8c0d5aaf51bb00d59863cb
dezkareid <elmaildeldezkareid@gmail.com>
2014-09-22 00:44:57 -0500

    Swiftstart
    Playing with Swift

---
+++ GDGPlay.playground/section-1.swift
@@ -0,0 +1,25 @@
+// Playground - Exploremos características de Swift jugando :3
+/**
+Author: Joel Humberto Gómez Paredes
+Group: GDG Monterrey
+*/
+import UIKit
+
+//Con var definimos una variable, las variables definen el tipo mas adecuado de acuerdo al valor asignado solo la primera vez
+var GDG_Monterrey_Saludo = "Hola amigos del GDG"
+
+//Una vez definido el tipo no puedes cambiarlo, descomenta y ve el error
+//saludo = 1
+
+//También podemos definir el tipo de dato desde un principio
+var tipoDefinido : Int = 1
+
+//En swift no necesitamos ; hell yes, sin embargo podemos usar ; para separar instrucciones en una misma linea
+var valor1 = 1; var valor2 = 2
+
+//Con let definimos una constante
+let start = "Swiftastic"
+
+//Aqui marca error porque no podemos cambiar el valor de una constante, descomenta y ve el error
+//start = "Otro mensaje"
+
